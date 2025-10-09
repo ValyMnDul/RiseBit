78fccde6ce61412ae4c563c894f10e4148c171ee
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-10-09 16:10:24 +0300

    I started the main page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <div className="h-[60px] w-[100%] bg-gray-800 flex flex-row justify-between items-center">
+            <div>POZA</div>
+            <input className="h-[40px] w-[600px] bg-gray-400"></input>
+            <div>LOGIN/Register</div>
+        </div>
+    )
+}
