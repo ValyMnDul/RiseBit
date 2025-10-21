330eebe5907928fc8ca25b580ebe185de846e0cc
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-10-21 23:46:04 +0300

    Add validation for email and username

--- src/app/api/(account)/change_email/route.ts
+++ src/app/api/(account)/change_email/route.ts
@@ -4,6 +4,25 @@ import { NextResponse } from "next/server"
 export const POST= async (req:Request)=>{
     const {email,newEmail} = await req.json();
 
+    const user = await prisma.user.findUnique({
+        where:{
+            email:email
+        }
+    })
+
+    if(!user){
+        return NextResponse.json({message:"User not found"},{status:404})
+    }
+
+    if(newEmail.length < 5 || newEmail.length > 200){
+        return NextResponse.json({message:"Email length invalid"},{status:400})
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if(!emailRegex.test(newEmail)){
+        return NextResponse.json({message:"Invalid email format"},{status:400})
+    }
+
     try{
         await prisma.user.update({
             where:{
--- src/app/api/(account)/change_username/route.ts
+++ src/app/api/(account)/change_username/route.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 export const POST = async (req: Request) => {
     const {newUsername, email} = await req.json();
 
+    if(newUsername.length < 3 || newUsername.length > 20){
+        return NextResponse.json({},{status:400});
+    }
+
+
     try{
             await prisma.user.update({
             where: {
