5110789a4d586cf096ae9185d32d6479aa357936
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-10-26 17:34:57 +0200

    build error fix 3

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'next/core-web-vitals',
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    '.next/',
+    'dist/',
+    'build/',
+    'out/',
+    'src/generated/',
+  ],
+  rules: {
+    
+  },
+};
