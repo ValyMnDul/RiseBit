6f639a77f5d4d3fed76d9edee648c3082767d694
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-11-09 16:36:15 +0200

    Delete user photo from Cloudinary on account deletion

--- src/app/api/(account)/delete_account/route.ts
+++ src/app/api/(account)/delete_account/route.ts
@@ -1,19 +1,45 @@
 import { NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
+import cloudinary from "@/lib/cloudinary";
+import { URLToPublicId } from "@/lib/cloudinaryUrlToPublicId";
 
-export const DELETE=async (req:Request)=>{
+export const DELETE = async (req:Request)=>{
 
-    const {email}=await req.json();
+    const { email } = await req.json();
 
-    if(await prisma.user.findUnique({where:{email:email}})===null){
-        return NextResponse.json({message:"Email is required"},{status:400})
+    const user = await prisma.user.findUnique({
+        where:{
+            email:email,
+        },
+        select:{
+            profilePic:true,
+        }
+    });
+
+    if(user === null){
+        return NextResponse.json({message:"Email is required"},{status:400});
     }
 
     await prisma.user.delete({
         where:{
             email:email
         }
-    })
+    });
+
+    const profilePic = user.profilePic;
+    
+    if(profilePic){
+        const profilePicPublicId = URLToPublicId(profilePic);
+        
+        if(profilePicPublicId){
+            try{
+                await cloudinary.uploader.destroy(profilePicPublicId);
+            }
+            catch(e){
+                console.log(`Failed to delete the photo:${e}`)
+            }
+        }
+    }
 
     return NextResponse.json({message:"Account deleted successfully"},{status:200})
 }
